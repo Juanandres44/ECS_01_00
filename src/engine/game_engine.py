6f6312a79614c02333fcbfedf9710994781ab49e
Juan Andrés Santiago <ja.santiago@uniandes.edu.co>
2023-04-01 23:30:36 -0500

    practice

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,6 +1,28 @@
+import pygame
+import esper
+
+from src.create.prefab_creator import crear_cuadrado
+
+from src.ecs.systems.s_screen_bounce import system_screen_bounce
+from src.ecs.systems.s_movement import system_movement
+from src.ecs.systems.s_rendering import system_rendering
+
+from src.ecs.components.c_surface import CSurface
+from src.ecs.components.c_transform import CTransform
+from src.ecs.components.c_velocity import CVelocity
+
+
+
 class GameEngine:
     def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((640,360), pygame.SCALED)
+        self.clock = pygame.time.Clock()
         self.is_running = False
+        self.framerate = 60
+        self.delta_time = 0
+
+        self.ecs_world = esper.World()
 
     def run(self) -> None:
         self._create()
@@ -13,19 +35,32 @@ class GameEngine:
         self._clean()
 
     def _create(self):
-        pass
+        crear_cuadrado(self.ecs_world, 
+                        pygame.Vector2(50,50),
+                        pygame.Vector2(150,300),
+                        pygame.Vector2(-200,300),
+                        pygame.Color(255,100,100))
+
 
     def _calculate_time(self):
-        pass
+        self.clock.tick(self.framerate)
+        self.delta_time = self.clock.get_time() / 1000.0
 
     def _process_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
 
     def _update(self):
-        pass
+        system_movement(self.ecs_world, self.delta_time)
+        system_screen_bounce(self.ecs_world, self.screen)
 
     def _draw(self):
-        pass
+        self.screen.fill((100,50,128))
+
+        system_rendering(self.ecs_world, self.screen)
+
+        pygame.display.flip()
 
     def _clean(self):
-        pass
+        pygame.quit()
